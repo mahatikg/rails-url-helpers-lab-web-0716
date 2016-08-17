65fbb3ecc0a28c0a810dd995b8224e20c671fd31
Mahati Gollamudi <github email address>
2016-08-17 19:29:47 -0400

    Add mvc functionality and tinker to pass tests

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,23 @@
+require 'pry'
 class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
-end
+
+  def activate
+    # binding.pry
+      if self.active
+        'active'
+        # "This student is currently active."
+      else
+        # "This student is currently inactive."
+        'inactive'
+      end
+    end
+
+    def change_status
+      self.active = !self.active
+      self.save
+    end
+
+  end
--- db/migrate/001_create_students.rb
+++ db/migrate/001_create_students.rb
@@ -1,10 +1,13 @@
 class CreateStudents < ActiveRecord::Migration
+
+
   def change
     create_table :students do |t|
       t.string :first_name
       t.string :last_name
-
+      t.boolean :active, :default => false
       t.timestamps null: false
     end
+    
   end
 end
